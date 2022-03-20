9bd0504f76491a790632805b5d87df699247c45c
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-20 16:52:30 +0300

    Spawn modified, collider generated

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -7,22 +7,34 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Spawn animal after 2 sc from the start of the key and every 1.5 sc
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //Randomly generate animal index and  spwan position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
-        }
+        //Sapwn an animal when we press S key
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    SpawnRandomAnimal();
+        // }
         
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Randomly generate animal index and  spwan position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
     }
 }
